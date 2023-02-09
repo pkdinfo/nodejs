a9add0116c5f9a91c083a14ee09ad89d2a9ad4b7
pravin <pravin@piggyride.in>
2023-02-09 16:37:36 +0530

    first commit

---
+++ data.js
@@ -0,0 +1,2 @@
+const data = [ { name: 'Pravin', email: 'pravin@gmail.com' }, { name: 'Swastik', email: 'swastik@gmail.com' } ];
+module.exports = data;
---
+++ index.js
@@ -0,0 +1,34 @@
+const path = require('path');
+const http = require('http');
+const fs = require('fs');
+
+const dirPath = path.join(__dirname, 'files');
+//console.warn(dirPath);
+for(i=0; i<5; i++)
+{
+    fs.writeFileSync(dirPath+'/hello'+i+'.txt','Simple Texty');
+}
+
+
+
+
+
+
+
+//==========================================================================
+//const path = require('path');
+//const dirPath = path.join(__dirname, 'files');
+// const http = require('http');
+// const data = require('./data');
+// const http = require('colors');
+// //fs.writeFileSync("helo.txt", "coding ");
+// console.log("Pravin" .red);
+// http
+// 	.createServer((req, resp) => {
+// 		resp.writeHead(200, { 'Content-Type': 'application/json' });
+// 		resp.write(JSON.stringify(data));
+// 		resp.end();
+// 	})
+// 	.listen(7000);
+
+//console.log('my name is pravin');
